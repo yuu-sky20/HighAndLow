687c3dc49c2353801eb0e8b6dedce11fa25220eb
yuu_sky <yuusky20th@gmail.com>
2023-02-05 22:45:05 +0900

    プロジェクト ファイルを追加します。

---
+++ HighAndLowApp/HighAndLow/GameController.cs
@@ -0,0 +1,55 @@
+﻿using static HighAndLowApp.HighAndLow.TrumpModel;
+
+namespace HighAndLowApp.HighAndLow
+{
+	public class GameController
+	{
+		private Trump enemyCard = new Trump();
+		private Trump myCard = new Trump();
+
+
+		public (Trump enemyCard, Trump myCard) GenerateCards()
+		{
+			Random r = new Random();
+			int enemyMark = r.Next(0, 4);
+			int enemyNumber = r.Next(1, 13);
+			int myMark = r.Next(0, 4);
+			int myNumber = r.Next(1, 13);
+			while ((enemyMark == myMark) && (enemyNumber == myNumber))
+			{
+                myMark = r.Next(0, 4);
+                myNumber = r.Next(1, 13);
+			}
+
+			enemyCard = new Trump()
+			{
+				Mark = (TrumpMark)enemyMark,
+				Number = (TrumpNumber)enemyNumber
+			};
+			myCard = new Trump()
+			{
+				Mark = (TrumpMark)myMark,
+				Number = (TrumpNumber)myNumber
+			};
+
+			return (enemyCard, myCard);
+		}
+
+		public int JudgeCards()
+		{
+			if (enemyCard.Number < myCard.Number)
+			{
+				return 1;
+			} else if (enemyCard.Number > myCard.Number)
+			{
+				return -1;
+			} else if (enemyCard == myCard)
+			{
+				return 0;
+			} else
+			{
+				return 2;
+			}
+		}
+	}
+}
---
+++ HighAndLowApp/HighAndLow/TrumpModel.cs
@@ -0,0 +1,53 @@
+﻿namespace HighAndLowApp.HighAndLow
+{
+	public class TrumpModel
+	{
+        public class Trump : IComparer<Trump>
+        {
+            public TrumpMark Mark { get; set; }
+            public TrumpNumber Number { get; set; }
+
+            public int Compare(Trump? a, Trump? b)
+            {
+                if (a == null || b == null)
+                {
+                    return 2;
+                }
+                if (a.Number > b.Number)
+                {
+                    return 1;
+                } else if (a.Number < b.Number)
+                {
+                    return -1;
+                } else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public enum TrumpMark
+        {
+            s = 0,
+            h = 1,
+            c = 2,
+            d = 3
+        };
+
+        public enum TrumpNumber
+        {
+            A = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            J = 10,
+            Q = 11,
+            K = 12
+        };
+	}
+}
